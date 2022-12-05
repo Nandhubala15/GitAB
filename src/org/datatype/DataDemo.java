a832310c1b3c17a0636049feb65b8a156d67df31
Nandhubala@15 <nandhuvb1997@gmail.com>
2022-12-05 16:35:15 +0530

    DataType

---
+++ src/org/datatype/DataDemo.java
@@ -0,0 +1,49 @@
+package org.datatype;
+
+public class DataDemo {
+
+	public static void main(String[] args) {
+	
+		//datatype variablename = value
+		
+	    System.out.println("==================");
+	
+		byte age =20;
+		System.out.println("Girl age is = " + age);
+		
+		short pinNo = 4454;
+	    System.out.println("ATM PinNo is = " + pinNo);
+		
+		int regno = 56757;
+	    System.out.println("student regNo is = " + regno);
+	    
+	    long phNo = 9047922897l;
+	    System.out.println("Student Phone Number is = " + phNo);
+	    
+	    float avg = 56.98f;
+	    System.out.println("Mark Average is = " + avg);
+	    
+	    double billno = 67.89d;
+	    System.out.println("Student Bill no is = " + billno);
+	    
+	    char gender = 'F';
+		System.out.println("Student Gender is = " + gender);
+		
+		String email = "nandhuvb1997@gmail.com";
+		System.out.println("Student email address is = " + email);
+		
+		boolean result = true;
+		System.out.println("All the above results are " + result);
+		
+	    System.out.println("=================="); 
+		
+		
+	
+		
+		
+	    
+	    
+
+	}
+
+}
